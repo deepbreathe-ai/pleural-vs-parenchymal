8b7339c9500547f0979968806cfc67b917bc964d
Bennett <bennettjlvb@gmail.com>
2022-02-17 20:23:30 -0500

    Cleaned up clip prediction script.

--- src/experiments/clip_pred_tuning.py
+++ src/experiments/clip_pred_tuning.py
@@ -8,7 +8,8 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 import multiprocessing
 
-from src.predict import max_contiguous_pleural_preds, max_sliding_window, compute_metrics, longest_window
+from src.predict import max_contiguous_pleural_preds, max_sliding_window, compute_metrics, \
+    longest_window, majority_vote, avg_clip_prediction
 from src.visualization.visualization import plot_clip_pred_experiment
 
 cfg = yaml.full_load(open(os.getcwd() + "/config.yml", 'r'))
@@ -154,12 +155,51 @@ def explore_longest_window(frame_preds, min_certainty=0.05, max_certainty = 0.4)
                               x_label='Window Size',
                               experiment_type='longest-window')
 
-def full_experiment(frame_preds):
-    explore_longest_window(frame_preds, 0.05, 0.4)
-    explore_sliding_window(frame_preds, 5, 60)
-    explore_contiguity_threshold(frame_preds, 0.5, 5, 60)
+def report_constant_clip_metrics(frame_preds):
+    '''
+    Plots accuracy of clip prediction algorithms that do not have variables to explore.
+    (Including majority vote and average probability)
+    :param frame_preds: DataFrame containing frame prediction probabilities
+    '''
+    frames_table_path = cfg['PATHS']['TEST_FRAMES_TABLE']
+    clips_table_path = cfg['PATHS']['TEST_CLIPS_TABLE']
+
+    frames_df = pd.read_csv(frames_table_path)
+    clips_df = pd.read_csv(clips_table_path)
+
+    clip_names = clips_df['filename']
+    clip_labels = clips_df['Class']
+    clip_pred_classes_avg = []
+    clip_pred_classes_majority = []
+    for i in tqdm(range(len(clip_names)), position=0, leave=True):
+        clip_name = clip_names[i]
+        clip_files_df = frames_df[frames_df['Frame Path'].str.contains(clip_name)]
+        if clip_files_df.shape[0] == 0:
+            raise Exception("Clip {} had 0 frames. Aborting.".format(clip_name))
+        clip_frame_preds = frame_preds[frame_preds['Frame Path'].str.contains(clip_name)]
+        pred_probs = clip_frame_preds['Pleural View Probability'].to_numpy()
+        clip_pred_classes_avg.append(avg_clip_prediction(pred_probs, cfg['CLIP_PREDICTION']['CLASSIFICATION_THRESHOLD'])[0])
+        clip_pred_classes_majority.append(majority_vote(pred_probs, cfg['CLIP_PREDICTION']['CLASSIFICATION_THRESHOLD']))
+    metrics_avg = compute_metrics(np.array(clip_labels), np.array(clip_pred_classes_avg))
+    metrics_majority = compute_metrics(np.array(clip_labels), np.array(clip_pred_classes_majority))
+    print("Average Clip Prediction Accuracy: {}".format(metrics_avg['accuracy']))
+    print("Majority Vote Clip Prediction Accuracy: {}".format(metrics_majority['accuracy']))
+
+
+def full_experiment(frame_preds, experiment_cfg):
+    explore_longest_window(frame_preds,
+                           experiment_cfg['LONGEST_WINDOW_CERTAINTIES'][0],
+                           experiment_cfg['LONGEST_WINDOW_CERTAINTIES'][1])
+    explore_sliding_window(frame_preds,
+                           experiment_cfg['SLIDING_WINDOW_LENGTHS'][0],
+                           experiment_cfg['SLIDING_WINDOW_LENGTHS'][1])
+    explore_contiguity_threshold(frame_preds,
+                                 experiment_cfg['CONTIGUITY_THRESHOLD_CLASS_THRESH'],
+                                 experiment_cfg['CONTIGUITY_THRESHOLD_RANGE'][0],
+                                 experiment_cfg['CONTIGUITY_THRESHOLD_RANGE'][1])
+    report_constant_clip_metrics(frame_preds)
 
 if __name__ == '__main__':
     frame_preds = pd.read_csv(cfg['PATHS']['FRAME_PREDICTIONS'])
-    #run an experiment on all clip prediction algorithms
-    full_experiment((frame_preds))
+    # run an experiment on all clip prediction algorithms
+    full_experiment(frame_preds, cfg['CLIP_PREDICTION']['EXPERIMENTS'])
