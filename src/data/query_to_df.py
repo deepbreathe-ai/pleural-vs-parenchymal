af6424bcaad7045bc51eae7c6a26de729e6492d2
bl21812 <brian.li21812@gmail.com>
2022-08-04 11:33:44 -0400

    Small formatting/cleaning updates to query_to_df.py

--- src/data/query_to_df.py
+++ src/data/query_to_df.py
@@ -9,7 +9,7 @@ database_query = cfg['PATHS']['DATABASE_QUERY']
 mask_clip_path = cfg['PATHS']['MASKED_CLIPS_DIR']
 
 
-def create_ABline_dataframe(database_query):
+def create_frame_dataframe(database_query):
     '''
     Extracts out pertinent information from database query csv and builds a dataframe linking filenames, patients, and class
     :database_filename: filepath to database query csv
@@ -24,7 +24,7 @@ def create_ABline_dataframe(database_query):
 
     df['exam_id'] = df['exam_id'].astype(str)
     df['patient_id'] = df['patient_id'].astype(str)
-    df['vid_id'] = df['vid_id'].astype(str) 
+    df['vid_id'] = df['vid_id'].astype(str)
 
     # Create filename
     df['filename'] = df['exam_id'] + "_" + df['patient_id'] + "_VID" + df['vid_id']
@@ -38,18 +38,13 @@ def create_ABline_dataframe(database_query):
 
     # Create column of class category to each clip.
     # Modifiable for binary or multi-class labelling
-    df['class'] = df.apply(lambda row: 0 if row['view'] == 'parenchymal' else
-    (1 if row['view'] == 'pleural' else
-     -1), axis=1)
+    df['class'] = df.apply(lambda row: 0 if row['view'] == 'parenchymal' else (1 if row['view'] == 'pleural' else -1),
+                           axis=1)
 
     df['Path'] = df.apply(lambda row: row.filename, axis=1)
 
     df['s3_path'] = df.apply(lambda row: row.s3_path, axis=1)
 
-    # Finalize dataframe
-    # df = df[['filename'] + COLUMNS_WANTED + ['class'] + ['Path'] + ['s3_path']]
-
-    # Save df - append this csv to the previous csv 'clips_by_patient_2.csv'
     df.to_csv(cfg['PATHS']['CLIPS_TABLE'], index=False)
 
     return df
@@ -68,10 +63,8 @@ def create_fold_dataframe(clips_table_path,fold):
     return fold_table_df
 
 
-# print(create_ABline_dataframe("parenchymal_clips.csv"))
-
 if __name__ == "__main__":
-    create_ABline_dataframe(database_query)
+    create_frame_dataframe(database_query)
 
     # for fold in range(10):
     #     create_fold_dataframe('data/train_query.csv',fold)
